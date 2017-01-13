347da758e6b65f04d9992978e98271de81aa0ce2
Jan Riedler <jan.riedler1998@gmx.at>
2017-01-13 15:26:51 +0100

    junit-Test

---
+++ junit-example/src/at/htlwels/riedler/Calculator.java
@@ -0,0 +1,10 @@
+package at.htlwels.riedler;
+
+public class Calculator {
+	  public int evaluate(String expression) {
+	    int sum = 0;
+	    for (String summand: expression.split("\\+"))
+	      sum += Integer.valueOf(summand);
+	    return sum;
+	  }
+	}
